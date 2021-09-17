875da61b990ca2e670ecb988eb5c95d9159a9ba7
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2021-09-17 07:40:48 -0300

    refactor lua script and enhance isInARace logic

--- Top Gear (USA).lua
+++ Top Gear (USA).lua
@@ -1,23 +1,31 @@
+---@diagnostic disable: lowercase-global, undefined-global
+
 function initializeScreenConstants()
-    padding = 15
-    circleBorderColor = "gray"
-    circleSize = 4
-    lapLineSize = 4
+    PADDING = 15
+    CIRCLE_BORDER_COLOR = "gray"
+    CIRCLE_SIZE = 4
+    LAP_LINE_SIZE = 4
 end
 
-function initializeCarColors()
-    carColor = {}
-    carColor[0] = "crimson"
-    carColor[1] = "white"
-    carColor[2] = "darkmagenta"
-    carColor[3] = "dodgerblue"
+function initializeGameConstants()
+    CAR_COLOR = {}
+    CAR_COLOR[0] = "crimson"
+    CAR_COLOR[1] = "white"
+    CAR_COLOR[2] = "darkmagenta"
+    CAR_COLOR[3] = "dodgerblue"
+    MAX_FUEL = 43007
+    LAST_FRAME_OF_COUNTDOWN_RANGE_START = 200
+    LAST_FRAME_OF_COUNTDOWN_RANGE_END = 600
+end
+
+function updatePreviousTrackFrameCounter()
+    previousTrackFrameCounter = memoryTrackFrameCounter
 end
 
 function initializeFuelConsumptionVariables()
-    totalFuel = 43007
-    lastFuel = {totalFuel,totalFuel}
+    lastFuel = {MAX_FUEL,MAX_FUEL}
     fuelConsumption = {{},{}}
-    memoryFuel = {totalFuel,totalFuel}
+    memoryFuel = {MAX_FUEL,MAX_FUEL}
 end
 
 function updateScreenSize()
@@ -27,7 +35,7 @@ function updateScreenSize()
     infoYPosition = {screenHeight / 4, screenHeight / 4 * 3}
 
     halfHeight = client.bufferheight() / 2
-    lineLength = client.bufferwidth() - 2 * padding
+    lineLength = client.bufferwidth() - 2 * PADDING
 end
 
 function clearScreen()
@@ -43,6 +51,8 @@ function readMemoryData()
     memoryLapSize = memory.read_u16_le(0x0200)
     memoryTrackFrameCounter = memory.read_u16_le(0x1EAE)
     memoryNumberOfLaps = memory.read_u16_le(0x1E56)
+    memoryRaceCountdownOn = memory.read_u16_le(0x1E58)
+    memorySpeed = getMemoryDataForBothPlayers(0x1E5A, 2)
     memoryEngine = getMemoryDataForBothPlayers(0x1E5E, 2)
     memoryFuel = getMemoryDataForBothPlayers(0x1E62, 2)
     memoryLap = getMemoryDataForBothPlayers(0x1E76, 2)
@@ -57,24 +67,27 @@ function drawPlayerInfo(player)
     local x = infoXPosition
     local y = infoYPosition[player]
     local lineBreakSize = 15
-    
+ 
     local fuel = memoryFuel[player]
-    local fuelRatio = fuel / totalFuel
+    local fuelRatio = fuel / MAX_FUEL
 
     local engine = memoryEngine[player]
+
+    local speed = memorySpeed[player]
 
     fuelText = 'Fuel: ' .. fuel
     fuelRatioText = 'Fuel ratio: ' .. fuelRatio
     engineText = 'Engine: ' .. engine
+    speedText = 'Speed: ' .. speed
 
     gui.text(x, y, fuelText)
     gui.text(x, y + lineBreakSize, fuelRatioText)
     gui.text(x, y + lineBreakSize * 2, engineText)
+    gui.text(x, y + lineBreakSize * 3, speedText)
 end
 
-
 function drawCompletionGraphLine(numberOfLaps,lapSize)
-    gui.drawLine(padding, halfHeight, padding + lineLength, halfHeight)
+    gui.drawLine(PADDING, halfHeight, PADDING + lineLength, halfHeight)
 end
 
 function drawCompletionGraphLaps(numberOfLaps,lapSize)
@@ -83,8 +96,8 @@ function drawCompletionGraphLaps(numberOfLaps,lapSize)
     do
         local currentLapPosition = i * lapSize
         local lapPercentage = currentLapPosition / total
-        local lapX = padding + lineLength * lapPercentage
-        gui.drawLine(lapX, halfHeight - lapLineSize / 2, lapX, halfHeight + lapLineSize / 2)
+        local lapX = PADDING + lineLength * lapPercentage
+        gui.drawLine(lapX, halfHeight - LAP_LINE_SIZE / 2, lapX, halfHeight + LAP_LINE_SIZE / 2)
     end
 end
 
@@ -94,9 +107,9 @@ function drawPlayerCircle(player, lapSize, numberOfLaps)
     local current = currentLap * lapSize + currentPositionOnTrack
     local total = numberOfLaps * lapSize
     local percentage = current / total
-    local circleCenter = padding + lineLength * percentage
+    local circleCenter = PADDING + lineLength * percentage
     local car = memoryCar[player]
-    gui.drawEllipse(circleCenter - circleSize / 2, halfHeight - circleSize / 2, circleSize, circleSize, circleBorderColor, carColor[car])
+    gui.drawEllipse(circleCenter - CIRCLE_SIZE / 2, halfHeight - CIRCLE_SIZE / 2, CIRCLE_SIZE, CIRCLE_SIZE, CIRCLE_BORDER_COLOR, CAR_COLOR[car])
 end
 
 function drawCompletionGraphPlayers(numberOfLaps,lapSize)
@@ -115,38 +128,46 @@ function drawCompletionGraph()
 end
 
 function updateConsumption()
-    local currentFrame = emu.framecount()
+    local currentRaceFrame = memoryTrackFrameCounter
     local MAX_ACCEPTED_DIFFERENCE = 1000
     for i = 1,2
-    do 
+    do
         local difference = lastFuel[i] - memoryFuel[i]
         if(difference > 0 and difference < MAX_ACCEPTED_DIFFERENCE)
         then
             lastFuel[i] = memoryFuel[i]
-            fuelConsumption[i][currentFrame] = difference
+            fuelConsumption[i][currentRaceFrame] = difference
         end
     end
 end
 
-function isInCountdown(frameCounter)
-    local LAST_FRAME_OF_COUNTDOWN_RANGE_START = 200
-    local LAST_FRAME_OF_COUNTDOWN_RANGE_END = 600
-    return frameCounter > LAST_FRAME_OF_COUNTDOWN_RANGE_START and frameCounter < LAST_FRAME_OF_COUNTDOWN_RANGE_END
+function isInCountdown()
+    return memoryRaceCountdownOn == 1
+end
+
+function isInFrameJustAfterCountdown(currentFrameCounter, previousFrameCounter)
+    return currentFrameCounter == 0
+    and previousFrameCounter > LAST_FRAME_OF_COUNTDOWN_RANGE_START
+    and previousFrameCounter < LAST_FRAME_OF_COUNTDOWN_RANGE_END
 end
 
 function isInARace()
     local currentTrackFrameCounter = memoryTrackFrameCounter
-    local response = false
-    if(lastTrackFrameCounter ~= currentTrackFrameCounter)
-    then
-        response = currentTrackFrameCounter > 0 or isInCountdown(lastTrackFrameCounter)
-        lastTrackFrameCounter = currentTrackFrameCounter
-    end
+    local frameDifference = currentTrackFrameCounter - previousTrackFrameCounter
+    local response =  isInCountdown()
+    or isInFrameJustAfterCountdown(currentTrackFrameCounter, previousTrackFrameCounter)
+    or frameDifference == 1
+
+    -- if(response) then gui.text(100,100,"IN A RACE") else gui.text(100,100,"NOT IN A RACE") end
+    -- gui.text(100,115,currentTrackFrameCounter)
+    -- gui.text(100,130,previousTrackFrameCounter)
+    -- if(isInCountdown()) then gui.text(100,145,"COUNTING DOWN") else gui.text(100,145,"NOT COUNTING DOWN") end
+
     return response
 end
 
 function initializeEverything()
-    initializeCarColors()
+    initializeGameConstants()
     initializeScreenConstants()
     initializeFuelConsumptionVariables()
     updateScreenSize()
@@ -155,9 +176,9 @@ end
 
 function gameLoop()
     while true do
+        readMemoryData()
         updateScreenSize()
         clearScreen()
-        readMemoryData()
         if(isInARace())
         then
             drawCompletionGraph()
@@ -165,7 +186,11 @@ function gameLoop()
             do
                 drawPlayerInfo(i)
             end
+        else
+            initializeFuelConsumptionVariables()
         end
+
+        updatePreviousTrackFrameCounter()
         emu.frameadvance()
     end
 end
